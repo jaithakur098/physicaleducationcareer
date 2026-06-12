c179bc9004cc34879ddfe84dc8f53aa1414d9110
jaithakur098 <phe.edu08@gmail.com>
2026-06-12 21:00:25 +0530

    Update class12-ch1-questions.js

--- class12-ch1-questions.js
+++ class12-ch1-questions.js
@@ -1,1 +1,37 @@
 
+const questions = [
+
+{
+question:"Intramural competitions are conducted within?",
+options:[
+"School",
+"District",
+"State",
+"Country"
+],
+answer:"School"
+},
+
+{
+question:"Knock-out tournament is also called?",
+options:[
+"League",
+"Round Robin",
+"Elimination Tournament",
+"Challenge"
+],
+answer:"Elimination Tournament"
+},
+
+{
+question:"League tournament is known as?",
+options:[
+"Round Robin",
+"Knock-out",
+"Challenge",
+"Bye System"
+],
+answer:"Round Robin"
+}
+
+];
